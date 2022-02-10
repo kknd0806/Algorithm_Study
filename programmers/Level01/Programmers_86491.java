005bd71bfc6e987f30b7773d227710983357984e
lee.jaehun <kknd0806@gmail.com>
2022-02-10 17:25:26 +0900

    Programmers_86491 문제풀이

--- programmers/Level01/Programmers_86491.java
+++ programmers/Level01/Programmers_86491.java
@@ -8,12 +8,24 @@ public class Programmers_86491 {
         solution(sizes);
     }
     public static int solution(int[][] sizes){
-        int answer = 0;
+        int height = 0;
+        int width = 0;
         for(int[] i : sizes){
             Arrays.sort(i);
-            System.out.println(i[0] + ", " + i[1] );
+            if(height<i[0]) height=i[0];
+            if(width<i[1]) width=i[1];
         }
 
-        return answer;
+        return height * width;
+    }
+    public static int solution2(int[][] sizes){
+        int height = 0;
+        int width = 0;
+        for(int[] i : sizes){
+            if(height<Math.max(i[0],i[1])) height=Math.max(i[0],i[1]);
+            if(width<Math.min(i[0],i[1])) width=Math.min(i[0],i[1]);
+        }
+
+        return height * width;
     }
 }
