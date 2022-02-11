068ae08f8736c37903832c914e94774eddba3d62
lee.jaehun <kknd0806@gmail.com>
2022-02-11 09:47:19 +0900

    Programmers_12901 풀이 추가

--- programmers/Level01/Programmers_12901.java
+++ programmers/Level01/Programmers_12901.java
@@ -1,5 +1,8 @@
 package programmers.Level01;
 
+import java.util.Calendar;
+import java.util.Locale;
+
 public class Programmers_12901 {
     public String solution(int a, int b){
         String[] day = {"THU","FRI","SAT","SUN","MON","TUE","WED"};
@@ -9,5 +12,11 @@ public class Programmers_12901 {
             date += month[i];
         }
         return day[date%7];
+    }
+
+    // 프로그래머스 추가 답
+    public String getDatName(int month, int day) {
+        Calendar cal = new Calendar.Builder().setCalendarType("iso8601").setDate(2016, month-1, day).build();
+        return cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, new Locale("ko-KR")).toUpperCase();
     }
 }
