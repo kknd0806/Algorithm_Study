389876ccec135fe755e7a4fa50d459db61de0586
lee.jaehun <kknd0806@gmail.com>
2022-03-24 22:15:05 +0900

    updated

--- programmers/Level01/Programmers_12932.java
+++ programmers/Level01/Programmers_12932.java
@@ -1,10 +1,7 @@
 package programmers.Level01;
 
 public class Programmers_12932 {
-    public static void main(String[] args) {
-        solution(12345);
-    }
-    public static int[] solution(long n) {
+    public int[] solution(long n) {
         String s = Long.toString(n);
         
         int[] answer = new int[s.length()];
