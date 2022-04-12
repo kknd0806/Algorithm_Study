dca876f05a63542cb7c007b023875986d016504a
LeeJaehun <kknd0806@gmail.com>
2022-04-12 22:59:21 +0900

    Programmers_12926 문제풀이

---
+++ programmers/Level01/Programmers_12926.java
@@ -0,0 +1,27 @@
+package programmers.Level01;
+
+
+public class Programmers_12926 {
+    public String solution(String s, int n) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(c>='a' && c<='z'){
+                c = floorChar(c, 'z', n);
+                sb.append(c);
+            } else if (c>='A' && c<='Z'){
+                c = floorChar(c, 'Z', n);
+                sb.append(c);
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+    private char floorChar(char target, char compare, int n) {
+                int result = target + n;
+        return result<=compare? (char)result : (char)(result-26);
+        
+    }
+}
