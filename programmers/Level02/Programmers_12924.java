71355c197c909bdd7c636182909dba45a7313128
LeeJaehun <kknd0806@gmail.com>
2022-04-24 13:48:21 +0900

    Programmers_12924 문제풀이

--- programmers/Level02/Programmers_12924.java
+++ programmers/Level02/Programmers_12924.java
@@ -1,33 +1,24 @@
 package programmers.Level02;
 
 public class Programmers_12924 {
-    public static void main(String[] args) {
-        System.out.println(solution(15));
+    public int solution(int n) {
+        int answer =0;
 
-    }
-    public static int solution(int n) {      
-        class Solution {
-            public int solution(int n) {
-                int answer =0;
-        
-                for(int i=1;i<=n;i++){
-                    int inum=i;
-                    int add =0;
-                    while (true){
-                        add+=inum;
-        
-                        if(add==n){
-                            answer++;
-                            break;
-                        }else if(add>n){
-                            break;
-                        }
-                        inum++;
-                    }
+        for(int i=1;i<=n;i++){
+            int inum=i;
+            int add =0;
+            while (true){
+                add+=inum;
+
+                if(add==n){
+                    answer++;
+                    break;
+                }else if(add>n){
+                    break;
                 }
-                return answer;
+                inum++;
             }
         }
-        
+        return answer;
     }
 }
