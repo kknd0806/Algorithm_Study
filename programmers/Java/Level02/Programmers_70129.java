cade6252777bdc337cfe469410bb7160f3440012
LeeJaehun <kknd0806@gmail.com>
2022-09-23 14:27:51 +0900

    git add test

--- programmers/Java/Level02/Programmers_70129.java
+++ programmers/Java/Level02/Programmers_70129.java
@@ -3,6 +3,7 @@ package programmers.Java.Level02;
 public class Programmers_70129 {
     public static void main(String[] args) {
         solution("110010101001");
+        int a = 1;
     }
 
     private static int[] solution(String s) {
